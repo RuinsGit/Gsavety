5332c669f4acde59c1770143ba44888b8069b32d
RuinsGit <museyibli.ruhin@gmail.com>
2025-03-07 04:52:47 -0800

    gsafety

--- resources/views/back/includes/header.blade.php
+++ resources/views/back/includes/header.blade.php
@@ -1,28 +1,28 @@
-<header id="page-topbar" style="background-color: white; border-bottom: 2px solid #e0e0e0;">
+<header id="page-topbar" style="background-color:rgb(37, 37, 37); border-bottom: 2px solid rgb(54, 50, 50);">
     <div class="navbar-header">
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
                 <a href="{{ route('admin.dashboard') }}" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ asset('back/assets/images/logoblog.svg') }}" alt="logo-sm" height="25">
+                        <img src="{{ asset('back/assets/images/logo.png') }}" alt="logo-sm" height="25">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('back/assets/images/logoblog.svg') }}" alt="logo-dark" height="50">
+                        <img src="{{ asset('back/assets/images/logo.png') }}" alt="logo-dark" height="50">
                     </span>
                 </a>
 
                 <a href="{{ route('admin.dashboard') }}" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('back/assets/images/logoblog.svg') }}" alt="logo-sm-light"
+                        <img src="{{ asset('back/assets/images/logo.png') }}" alt="logo-sm-light"
                             height="25">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('back/assets/images/logoblog.svg') }}" alt="logo-light" height="50">
+                        <img src="{{ asset('back/assets/images/logo.png') }}" alt="logo-light" height="50">
                     </span>
                 </a>
             </div>
-            <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect" id="vertical-menu-btn" style="color: #000;">
+            <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect" id="vertical-menu-btn" style="color: white;">
                 <i class="ri-menu-2-line align-middle"></i>
             </button>
         </div>
@@ -31,9 +31,9 @@
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img width="80" src="{{ asset('back/assets/images/logoblog.svg') }}" alt="Header Avatar">
-                    <span class="d-none d-xl-inline-block ms-1" style="color: #000;">{{ auth()->guard('admin')->user()->name }}</span>
-                    <i class="mdi mdi-chevron-down d-none d-xl-inline-block" style="color: #000;"></i>
+                    <!-- <img width="80" src="{{ asset('back/assets/images/logo.png') }}" alt="Header Avatar"> -->
+                    <span class="d-none d-xl-inline-block ms-1" style="color: white;">{{ auth()->guard('admin')->user()->name }}</span>
+                    <i class="mdi mdi-chevron-down d-none d-xl-inline-block" style="color: white;"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- <a class="dropdown-item" href="{{ route('admin.profile') }}"><i class="ri-user-line align-middle me-1"></i> Profil</a> -->
--- resources/views/back/includes/sidebar.blade.php
+++ resources/views/back/includes/sidebar.blade.php
@@ -1,53 +1,53 @@
 <!-- ========== Left Sidebar Start ========== -->
-<div class="vertical-menu">
+<div class="vertical-menu" style="background-color: rgb(34, 33, 33);">
 
     <div data-simplebar class="h-100">
 
         <!-- User details -->
         <div class="user-profile text-center mt-3">
-            <div class="">
-                <img src="{{ asset('back/assets/images/logoblog.svg') }}" width="80" alt="">
-            </div>
+           
             <div class="mt-3">
-                <h4 class="font-size-16 mb-1">{{ auth()->user()->name }}</h4>
+                <h4 class="font-size-16 mb-1" style="color: white;">
+                    {{ auth()->guard('admin')->user()->name ?? 'Administrator' }}
+                </h4>
                 <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i>
                     Online</span>
             </div>
         </div>
 
         <!--- Sidemenu -->
-        <div id="sidebar-menu">
+        <div id="sidebar-menu" >
             <!-- Left Menu Start -->
-            <ul class="metismenu list-unstyled" id="side-menu">
-                <li class="menu-title">Menu</li>
+            <ul class="metismenu list-unstyled" id="side-menu" style="color: white; " >
+                <li class="menu-title" style="color: white; " >Menu</li>
 
                 <li>
-                    <a href="№" class="waves-effect">
-                        <i class="ri-dashboard-line"></i>
+                    <a href="{{ route('admin.dashboard') }}" class="waves-effect">
+                        <i class="ri-dashboard-line" style="color: white;"></i>
                         <span>Ana Səhifə</span>
                     </a>
                 </li>
 
                 <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-settings-3-line"></i>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect" style="background: #111; border-left: 3px solid #fff;">
+                        <i class="ri-settings-3-line" style="color: white;"></i>
                         <span>Tənzimləmələr</span>
                     </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                    <li>
-                            <a href="{{ route('back.pages.translation-manage.index') }}">
-                                <i class="ri-translate"></i>
-                                <span>Tərcümələr</span>
+                    <ul class="sub-menu" style="background: #111; border-left: 3px solid #fff;">
+                        <li>
+                            <a href="{{ route('back.pages.translation-manage.index') }}" 
+                               style="color: #ccc;">
+                                <i class="ri-translate"></i> Tərcümələr
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('back.pages.seo.index') }}">
+                            <a href="{{ route('back.pages.seo.index') }}" style="color: #ccc;">
                                 <i class="ri-earth-line"></i>
                                 <span>SEO</span>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('back.pages.logos.index') }}">
+                            <a href="{{ route('back.pages.logos.index') }}" style="color: #ccc;">
                                 <i class="ri-file-line"></i>
                                 <span>Logo</span>
                             </a>
@@ -58,28 +58,28 @@
                 </li>
 
                 <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-share-line"></i>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect" style="background: #111; border-left: 3px solid #fff;">
+                        <i class="ri-share-line" style="color: white;"></i>
                         <span>Sosial Media</span>
                     </a>
-                    <ul class="sub-menu" aria-expanded="false">
+                    <ul class="sub-menu" style="background: #111; border-left: 3px solid #fff;">
                        
                     <li>
-                            <a href="{{ route('back.pages.social.index') }}">
+                            <a href="{{ route('back.pages.social.index') }}" style="color: #ccc;">
                                 <i class="ri-messenger-line"></i>
                                 <span>Sosial Media</span>
                             </a>
                         </li>   
 
                         <li>
-                        <a href="{{ route('back.pages.socialshare.index') }}">
+                        <a href="{{ route('back.pages.socialshare.index') }}" style="color: #ccc;">
                             <i class="ri-share-line"></i>
                             <span>Sosial Share</span>
                         </a>
                     </li>  
 
                     <li>
-                        <a href="{{ route('back.pages.socialfooter.index') }}">
+                        <a href="{{ route('back.pages.socialfooter.index') }}" style="color: #ccc;">
                             <i class="ri-mail-open-line"></i>
                             <span>Sosial Footer</span>
                         </a>
@@ -88,6 +88,16 @@
                         
    
                     </ul>
+                </li>
+
+              
+                   
+                    </ul>
+                        
+                </li>
+
+              
+
             </ul>
         </div>
         <!-- Sidebar -->
